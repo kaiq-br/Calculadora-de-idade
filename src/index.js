5223049550f72259fe8958ca5bbfb78427701aef
kaiq-br <78987344+kaiq-br@users.noreply.github.com>
2024-11-12 21:14:35 -0300

    ✨feat: criação do javascript

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const birthdayEl = document.getElementById("birthday");
+const buttonEl = document.getElementById("btn");
+const resultEl = document.getElementById("result");
+
+function calculateAge(){
+    const birthdayValue = birthdayEl.value;
+    if(birthdayValue === ""){
+        alert('Insira uma data');
+    } else {
+        const age = getAge(birthdayValue);
+        resultEl.innerHTML = `Sua idade é ${age} ${age > 1 ? "years" : "year"}`;
+    }
+}
+
+function getAge(birthdayValue){
+    const currentDate = new Date();
+    const birthdayDate = new Date(birthdayValue);
+    let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+    const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+    if(month < 0 || (month === 0 && currentDate.getDate() < birthdayDate.getDate())){
+        age--;
+    }
+    return age;
+}
+
+buttonEl.addEventListener("click", calculateAge);
